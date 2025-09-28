e1077688b3554ce3c0f1e02aeee0a0644ab6aaf9
jay <jykim5204@gmail.com>
2025-09-28 08:24:40 +0000

    frame stack arg default: for backward compatibility

--- drqv2.py
+++ drqv2.py
@@ -148,7 +148,7 @@ class Critic(nn.Module):
 class DrQV2Agent:
     def __init__(self, obs_shape, action_shape, device, lr, feature_dim,
                  hidden_dim, critic_target_tau, num_expl_steps,
-                 update_every_steps, stddev_schedule, stddev_clip, use_tb, frame_stack):
+                 update_every_steps, stddev_schedule, stddev_clip, use_tb, frame_stack=3):
         self.device = device
         self.critic_target_tau = critic_target_tau
         self.update_every_steps = update_every_steps
